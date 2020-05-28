1820e0f4704a9fa0437427e908cd00f90ed34050
risae <saerisa.1108@gmail.com>
2020-05-28 16:07:20 +0900

    Updates

---
+++ precache-manifest.511ddc069a8d92ccfb336cb9d74852dd.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ccf6ab8f1b54bfca2bf1a5bc9cb894c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c58c6df07f35daebf1c6",
+    "url": "/static/css/main.3da04d1d.chunk.css"
+  },
+  {
+    "revision": "14999356e334511a8078",
+    "url": "/static/js/2.26da0d87.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.26da0d87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c58c6df07f35daebf1c6",
+    "url": "/static/js/main.cfa6716d.chunk.js"
+  },
+  {
+    "revision": "75f2c3a16d8bf3d90a5b",
+    "url": "/static/js/runtime-main.c8f6c182.js"
+  },
+  {
+    "revision": "049224cb4613b80529784d33e45b3cff",
+    "url": "/static/media/bg.049224cb.jpg"
+  },
+  {
+    "revision": "9985e923bcd4ce52b3834756c906d52c",
+    "url": "/static/media/chara0.9985e923.png"
+  },
+  {
+    "revision": "815911752586af7daac380523f40d44b",
+    "url": "/static/media/neteruyaro.81591175.m4a"
+  },
+  {
+    "revision": "297cb34502f043a50dbe135a7ec5a2f8",
+    "url": "/static/media/omurice.297cb345.m4a"
+  },
+  {
+    "revision": "3ac796aba56faf973a8b15c06d519e38",
+    "url": "/static/media/sugoiyan.3ac796ab.m4a"
+  }
+]);
